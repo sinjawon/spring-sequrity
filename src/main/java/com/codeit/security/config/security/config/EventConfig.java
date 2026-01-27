d4f1fd6387ba4683082534547951ee0175baa0f3
sinjawon <qlqhroal@naver.com>
2026-01-27 17:37:12 +0900

    로그인 성공 및 실패 이벤트 로깅 기능 추가

--- src/main/java/com/codeit/security/config/security/config/EventConfig.java
+++ src/main/java/com/codeit/security/config/security/config/EventConfig.java
@@ -1,12 +1,12 @@
 package com.codeit.security.config.security.config;
 
-import com.codeit.security.config.security.event.AuthenticationFailureListner;
+
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
-import org.springframework.stereotype.Controller;
+
 
 @Configuration
 public class EventConfig {
